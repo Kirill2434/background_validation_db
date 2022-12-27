28632305ebc19a3f81693413ab518690f1bd6278
Kirill2434 <cool.yn20@yandex.ru>
2022-12-27 15:10:56 +0300

    Rew_0.3

--- Parsing_excel/excel_merger.py
+++ Parsing_excel/excel_merger.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import PureWindowsPath
+from pathlib import PureWindowsPath, Path
 
 import pandas as pd
 import glob
@@ -11,22 +11,44 @@ NDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 NDS_IMPORT_TC = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 shtraf_119_NK_RF = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-FILE_NAME = 'C:\source_data\common_data_file.xlsx'
-FILE_NAME_shtraf_119_NK_RF = 'C:\source_data\common_data_shtraf_119_NK_RF_file.csv'
+FILE_NAME = 'C:\generation_results\common_data_file.xlsx'
+FILE_NAME_shtraf_119_NK_RF = 'C:\generation_results\common_data_shtraf_119_NK_RF_file.csv'
 
 main_folder = PureWindowsPath('C:\source_data')
 main_dir = os.chdir('C:\source_data')
 
-# Подсчет обрабатываемых файлов в рабочей папке
-print(f"В папке {main_folder} хранится {len(list(glob.glob('*.xlsx')))} объектов в формате .xlsx")
-
-
+all_files = glob.glob('*.xlsx')
 files_nn_122 = glob.glob('???122.xlsx')
 files_nn = glob.glob('??.xlsx')
 files_nnn = glob.glob('???.xlsx')
 files_nnnn_nnn = glob.glob('99??????.xlsx')
 
+# Подсчет обрабатываемых файлов в рабочей папке
+print(f"В папке {main_folder} хранится {len(list(all_files))} объектов в формате .xlsx")
+
+
 files_list = [files_nn, files_nnn, files_nnnn_nnn]
+
+# Проверка кол-ва листов в книге
+incoorect_list_of_files = []
+correct_list_of_files_17 = []
+correct_list_of_files_18 = []
+for file in tqdm(all_files):
+    correct_sheet = ['налог на прибыль', 'НДС', 'НДС импорт ТС']
+    correct_sheet_2 = ['штраф 119 НК РФ']
+    xl = pd.ExcelFile(file)
+    sheets = xl.sheet_names
+
+    if sheets == correct_sheet:
+        correct_list_of_files_17.append(Path(xl).name)
+    elif sheets == correct_sheet_2:
+        correct_list_of_files_18.append(Path(xl).name)
+    else:
+        incoorect_list_of_files.append(Path(xl).name)
+
+print(f'Файлов с ошибкой: {len(incoorect_list_of_files)}\n список файлов: {incoorect_list_of_files}')
+print(f'Корректных файлов п.17: {len(correct_list_of_files_17)}\n список файлов: {correct_list_of_files_17}')
+print(f'Корректных файлов п.17: {len(correct_list_of_files_18)}\n список файлов: {correct_list_of_files_18}')
 
 if files_nn_122:
     '''Процесс формиования DataFrame'ов путем слияния набора Excel файлов'''
