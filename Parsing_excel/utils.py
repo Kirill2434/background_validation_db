6e19c67cc62bd9b080b234daa36644c476808831
Kirill2434 <cool.yn20@yandex.ru>
2023-03-29 17:58:13 +0300

    check_col_2

--- Parsing_excel/utils.py
+++ Parsing_excel/utils.py
@@ -1,36 +1,54 @@
 import glob
 import os
+from pathlib import Path
 from pprint import pprint
 
+import pandas as pd
 from pandas import ExcelWriter
 import openpyxl as op
 from prettytable import PrettyTable
+from tqdm import tqdm
 
-from FRPV.frpv_config import FRPV_CHECK_REPORT_N, FRPV_CHECK_REPORT
+from FRPV.frpv_config import FRPV_CHECK_REPORT_N, FRPV_CHECK_REPORT, R_1, R_3
 from source.config import main_folder
 
 
-def head_of_table(file):
-    """Опредление шапки таблиц файлов Excel. """
-    wb = op.load_workbook(file)
-    ws = wb['Раздел 1']
+def head_of_table(file, sheet_name: str = 'Лист1'):
+    """
+
+    @param file:
+    @param sheet_name:
+    @return:
+    """
+    miss_files = []
     head_of_file = []
     num_of_col = 1
     index_of_head_row = 0
     count_of_none = 0
-    for col in ws.iter_rows(min_row=1, max_col=1, max_row=20):
-        none_list = []
-        for cell in col:
-            none_list.append(cell.value)
-            for empty_cell in none_list:
-                if empty_cell is None:
-                    count_of_none += 1
-                else:
-                    pass
-    if count_of_none == 20:
-        num_of_col += 1
-    else:
-        pass
+    num_of_none_col = 1
+    df = pd.ExcelFile(file)
+    file_name = Path(df).name
+    wb = op.load_workbook(file)
+
+    try:
+        ws = wb[sheet_name]
+    except KeyError:
+        ws = wb.active
+    while count_of_none == 0:
+        for col in ws.iter_rows(min_row=num_of_col, max_col=num_of_col, max_row=20):
+            none_list = []
+            for cell in col:
+                none_list.append(cell.value)
+                for empty_cell in none_list:
+                    if empty_cell is None:
+                        count_of_none += 1
+                    else:
+                        pass
+        if count_of_none == 20:
+            num_of_col += 1
+        else:
+            pass
+
     for row in ws.iter_cols(min_col=num_of_col, max_col=num_of_col):
         for cell in row:
             if cell.value is None:
@@ -47,31 +65,58 @@ def head_of_table(file):
                                 else:
                                     try:
                                         int_cell = int(cell_second.value)
-                                        if int_cell == 2:
-                                            number_of_rows = cell.row
-                                            index_of_head_row += number_of_rows
+                                        str_cell = str(cell_second.value)
+                                        if len(str_cell) == 1:
+                                            if int_cell == 2:
+                                                number_of_rows = cell.row
+                                                number_of_cols = cell.column
+                                                index_of_head_row += number_of_rows
+                                            else:
+                                                break
                                         else:
-                                            break
+                                            pass
                                     except ValueError:
                                         pass
                 except ValueError:
                     pass
 
-    for row in ws.iter_rows(min_row=index_of_head_row, max_row=index_of_head_row, values_only=True):
-        for cell in row:
-            if cell is None:
+    for row in ws.iter_rows(min_row=index_of_head_row, max_row=index_of_head_row):
+        try:
+            for cell in row:
+                if cell.value is None:
+                    pass
+                else:
+                    head_of_file.append(int(cell.value))
+        except ValueError:
+            miss_files.append(file_name)
+            return miss_files
+    return head_of_file
+
+
+print(head_of_table(r'C:\Приложения регионов_исход\3_14.03.2023 после 11\7200.xlsx'))
+
+
+def proverka_kolonok_ultimate_xxx(path, sum_of_head: int, *, sheet_name: str = 'Лист1'):
+    inc_dict = {}
+    final_dict = {}
+    sum_of_head = list(range(1, sum_of_head + 1))
+    try:
+        for file in tqdm(path):
+            df = pd.ExcelFile(file)
+            file_name = Path(df).name
+            head_func = head_of_table(file, sheet_name)
+            if head_func == sum_of_head:
                 pass
             else:
-                head_of_file.append(int(cell))
-    return head_of_file
-    # return 'Fin'
+                final_dict[file_name] = sheet_name
+    except FileNotFoundError:
+        return 'Файл не найден!'
+    return final_dict
 
 
-d = {}
-all = glob.glob(r'C:\ФРПВ_1 раздел\source_data\*.xlsx')
-for a in all:
-    d[a] = head_of_table(a)
-pprint(d, depth=1)
+# all_filas = glob.glob(r'C:\Приложения регионов_исход\3_14.03.2023 после 11\*.xlsx')
+# reault = proverka_kolonok_ultimate_xxx(all_filas, 36)
+# print(reault)
 
 
 def count_empty_rows():
